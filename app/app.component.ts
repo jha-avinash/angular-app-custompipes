e061effb051904313169730628914635b2282bb5
jha-avinash <avinash.jha.k@gmail.com>
2016-10-22 16:52:41 -0500

    Custome Pipes
    Simple Calculator Using Custom Pipes

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'power-boost-calculator',
+  templateUrl: './app/template.html'
+})
+export class AppComponent {
+  value1 = 5;
+  value2 = 1;
+  }
+ 
